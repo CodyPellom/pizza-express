a32533feabce8dbf6d5d831f5693134e6eade862
paulpellom@gmail.com <paulpellom@gmail.com>
2018-02-12 23:10:57 -0500

    completed GET routes

--- app.js
+++ app.js
@@ -10,6 +10,20 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
+app.get('/', function(req, res) {
+res.send("Welcome to express pizza!")
+
+})
+
+app.get('/topping/pepperoni', function(req, res){
+res.send("pepperoni pizza! Good choice!")
+
+})
+
+app.get('/order/:amount/:size', function(req, res){
+res.send("Your order for 10 medium pizzas will be ready in 1 min!")
+
+})
 // tells the server to listen for requests on port
 //3000 AKA localhost:3000 
 app.listen(PORT, function(){
